f952b62c3ca1afad3ef4b51c36ef6b88df9d3f0c
eduardog949 <eduardo.g949@outlook.com>
2024-11-09 15:45:00 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1;
+    countEl.innerText = count
+}
+
+function save(){
+    let saveCount = count + " - ";
+saveEl.textContent +=  saveCount;
+countEl.innerText = 0;
+count = 0;
+}
+  
+
+
